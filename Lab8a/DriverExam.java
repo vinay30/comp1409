ea5aeae3d4fd5eb41eeadf576c381dff5ca2720e
Vinay Punwani <vinay30@users.noreply.github.com>
2016-11-06 18:38:22 -0800

    Add files via upload

--- Lab8a/DriverExam.java
+++ Lab8a/DriverExam.java
@@ -13,7 +13,7 @@ public class DriverExam
     public static final int PASSING_GRADE = 15;
     
     // instance variables - replace the example below with your own
-    private String[] driver_answers;
+    private String[] driverAnswers;
     private InputReader inputAnswers;
 
     /**
@@ -22,7 +22,7 @@ public class DriverExam
     public DriverExam()
     {
         // initialise instance variables
-        driver_answers = new String[20];
+        driverAnswers = new String[20];
         inputAnswers = new InputReader();
     }
 
@@ -30,45 +30,46 @@ public class DriverExam
         int questionNumber = 0;
         
         for(int index = 0; index < ANSWERS.length; index++) {
-            questionNumber++;
+            questionNumber = index + 1;
             System.out.println("Enter an answer for question " + questionNumber);
             String answer = inputAnswers.readString().toUpperCase();
             if(checkValidInputs(answer, questionNumber)) {
-                driver_answers[index] = answer;
+                driverAnswers[index] = answer;
             } else {
-                System.out.println("Invalid answer for question " + questionNumber + "! Try again.");
                 index--;
-                questionNumber--;
             }
         }
         
         System.out.println("Thanks for completing the exam!");
+        System.out.println("You answered " + getTotalCorrectAnswers() + " questions correctly.");
+        System.out.println("You answered " + getTotalIncorrectAnswers() + " questions incorrectly.");
     }
     
     public Boolean checkValidInputs(String answer, int questionNumber) {        
         for(int choiceIndex = 0; choiceIndex < VALID_INPUTS.length; choiceIndex++) {
             if(answer != null && answer.equals(VALID_INPUTS[choiceIndex])) {
-                System.out.println("Valid answer saved for question " + questionNumber + "!");
+                System.out.println("Valid answer saved for question " + questionNumber);
                 return true;                
             }
         }
         
+        System.out.println("Invalid answer for question " + questionNumber + "! Try again.");
         return false;
     }
     
     public int getTotalCorrectAnswers() {
-        int correct = 0;
-        return checkExamAnswers(correct);
+        return checkExamAnswers();
     }
     
     public int getTotalIncorrectAnswers() {
-        int correct = 0;
-        return ANSWERS.length - checkExamAnswers(correct);
+        return ANSWERS.length - checkExamAnswers();
     }
     
-    private int checkExamAnswers(int correct) {
+    private int checkExamAnswers() {
+        int correct = 0;
+        
         for(int index = 0; index < ANSWERS.length; index++) {
-            if(driver_answers[index] != null && driver_answers[index].equals(ANSWERS[index])) {
+            if(driverAnswers[index] != null && driverAnswers[index].equals(ANSWERS[index])) {
                 correct++;
             }
         }
@@ -78,21 +79,17 @@ public class DriverExam
     
     public Boolean passOrFail() {
         if(getTotalCorrectAnswers() >= PASSING_GRADE) {
+            System.out.println("Congratulations! You pass!");
             return true;
         }
         
+        System.out.println("Please stay off the road. You failed.");
         return false;
     }
     
     public static void main(String[] args) {
         DriverExam currentExam = new DriverExam();
         currentExam.promptStudentAnswers();
-        System.out.println("You answered " + currentExam.getTotalCorrectAnswers() + " questions correctly!");
-        System.out.println("You answered " + currentExam.getTotalIncorrectAnswers() + " questions incorrectly!");
-        if(currentExam.passOrFail()) {
-            System.out.println("Congratulations! You pass!");
-        } else {
-            System.out.println("Please stay off the road. You failed!");
-        }
+        currentExam.passOrFail();
     }
 }
