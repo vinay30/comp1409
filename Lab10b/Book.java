6cc282adea5181950e83f97980f4d6afca9ae524
Vinay Punwani <vinay30@users.noreply.github.com>
2016-11-23 00:08:55 -0800

    Add files via upload

--- Lab10b/Book.java
+++ Lab10b/Book.java
@@ -1,9 +1,9 @@
 
 /**
- * Write a description of class Book here.
+ * Class describing the fields and methods used to store and retrieve book information
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Vinay Punwani
+ * @version v1.0 Nov 22, 2016
  */
 public class Book
 {
@@ -18,7 +18,7 @@ public class Book
      */
     public Book()
     {
-        // initialise instance variables
+        // initialise instance variables to defaults
         title = "";
         author = "";
         yearPublished = 0;
@@ -27,6 +27,11 @@ public class Book
     
     /**
      * Non-default Constructor for objects of class Book
+     *
+     * @param title A parameter containing the new title of the book
+     * @param author A parameter containing the new author of the book
+     * @param yearPublished A parameter containing the year of publication for the book
+     * @param bookPriceInCAD A parameter containing the book price in CAD
      */
     public Book(String title, String author, int yearPublished, double bookPriceInCAD)
     {
@@ -38,23 +43,49 @@ public class Book
     }
 
     //Accessor methods begin
+    /**
+     * Method getTitle which returns the title of the book
+     *
+     * @return The return value which is the title of the book as a string
+     */
     public String getTitle() {
         return title;
     }
     
+    /**
+     * Method getAuthor which returns the author of the book
+     *
+     * @return The return value which is the author of the book as a string
+     */
     public String getAuthor() {
         return author;
     }
     
+    /**
+     * Method getYearPublished which returns the publication year of the book
+     *
+     * @return The return value which is an int of the year published
+     */
     public int getYearPublished() {
         return yearPublished;
     }
     
+    /**
+     * Method getBookPriceInCAD which returns the book price in CAD
+     *
+     * @return The return value which is a double, the book price in CAD
+     */
     public double getBookPriceInCAD() {
         return bookPriceInCAD;
     }
     
     //Mutator methods begin
+    /**
+     * Method setTitle which validates then sets the title of the book from a passed in parameter. If the title
+     * is not valid, an error message will be displayed.
+     *
+     * @param title A parameter containing the book title as a string
+     */
     public void setTitle(String title) {
         if(title != null) {
             this.title = title;
@@ -65,6 +96,12 @@ public class Book
         }
     }
     
+    /**
+     * Method setAuthor which validates then sets the author of the book from a passed in parameter. If the author
+     * is not valid, an error message will be displayed.
+     *
+     * @param author A parameter containing the book author as a string
+     */
     public void setAuthor(String author) {
         if(author != null) {
             this.author = author;
@@ -75,6 +112,11 @@ public class Book
         }
     }
     
+    /**
+     * Method setYearPublished which validates and sets the publication year of the book from a passed in parameter.
+     *
+     * @param yearPublished A parameter containing the publication year of the book, an int
+     */
     public void setYearPublished(int yearPublished) {
         if(yearPublished > 0) {
             this.yearPublished = yearPublished;
@@ -85,6 +127,11 @@ public class Book
         }
     }
     
+    /**
+     * Method setBookPriceInCAD which validates and sets the book price in canadian dollars from a passed in parameter
+     *
+     * @param bookPriceInCAD A parameter containing the new book price, a double
+     */
     public void setBookPriceInCAD(double bookPriceInCAD) {
         if(bookPriceInCAD > 0.0) {
             this.bookPriceInCAD = bookPriceInCAD;
@@ -95,6 +142,10 @@ public class Book
         }
     }
     
+    /**
+     * Method displayBookDetails which displays all the fields of the book object on the screen
+     *
+     */
     public void displayBookDetails() {
         System.out.println("Title: " + title);
         System.out.println("Author: " + author);
--- Lab10b/BookStore.java
+++ Lab10b/BookStore.java
@@ -1,13 +1,13 @@
 import java.util.ArrayList;
 /**
- * Write a description of class BookStore here.
+ * Class describing the fields and methods used to operate a bookstore containing a list of books
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Vinay Punwani
+ * @version v1.0 Nov 22, 2016
  */
 public class BookStore
 {
-    // instance variables - replace the example below with your own
+    // instance variables
     private ArrayList<Book> booklist;
     private String businessName;
 
@@ -16,13 +16,15 @@ public class BookStore
      */
     public BookStore()
     {
-        // initialise instance variables
+        // initialise instance variables to default values
         businessName = "Book Store";
         booklist = new ArrayList<Book>();
     }
 
     /**
      * Non-default Constructor for objects of class BookStore
+     *
+     * @param businessName A parameter containing the new business name of the book store
      */
     public BookStore(String businessName)
     {
@@ -32,11 +34,31 @@ public class BookStore
     }
     
     //Accessor methods
+    /**
+     * Method getBusinessName which returns the business name of the book store
+     *
+     * @return The return value which is a string of the business name
+     */
     public String getBusinessName() {
         return businessName;
     }
+    
+    /**
+     * Method getBooklist which returns the arraylist containing all the book objects
+     *
+     * @return The return value which is the arrayList containing book objects
+     */
+    public ArrayList<Book> getBooklist() {
+        return booklist;
+    }
 
     //Mutator methods
+    /**
+     * Method setBusinessName which validates and sets the new business name of the book store from a passed in
+     * parameter. If the parameter is invalid, the business name is not changed.
+     *
+     * @param businessName A parameter containing the new business name
+     */
     public void setBusinessName(String businessName) {
         if(businessName != null) {
             this.businessName = businessName;
@@ -48,6 +70,12 @@ public class BookStore
     }
     
     //ArrayList methods
+    /**
+     * Method addBook which validates the passed in book object, then adds it to the end of the booklist arraylist
+     * If the book object fails validation, an error message is displayed
+     *
+     * @param book A parameter containing a book object to be added to the booklist arraylist
+     */
     public void addBook(Book book) {
         System.out.println("Attempting to add book to book list...");
         if(book != null) {
@@ -59,6 +87,13 @@ public class BookStore
         System.out.println();
     }
     
+    /**
+     * Method getBook which uses a passed in parameter containing an arraylist index to retrieve the object
+     * at the specified index from the booklist arraylist. The parameter is validated before being used.
+     * If the index in the parameter fails validation, an error message is displayed
+     *
+     * @param bookIndex A parameter containing an index value of the booklist arraylist
+     */
     public void getBook(int bookIndex) {
         System.out.println("Attempting to get details of the book at index " + bookIndex + " from the book list...");
         if(bookIndex >= 0 && bookIndex < booklist.size()) {
@@ -70,6 +105,13 @@ public class BookStore
         System.out.println();
     }
     
+    /**
+     * Method findBook which takes a passed in parameter and searches the booklist arraylist for the book object with
+     * a title that matches the parameter string. If the book is found, the book's details are displayed. If the
+     * book is not found, an error message is displayed.
+     *
+     * @param bookTitle A parameter containing the book title search query
+     */
     public void findBook(String bookTitle) {
         System.out.println("Searching book list for: " + bookTitle);
         for(Book currentBook: booklist) {
@@ -84,6 +126,11 @@ public class BookStore
         System.out.println();
     }
     
+    /**
+     * Method displayAllBooks which iterates through each book object in the booklist arraylist and then displays
+     * the details of each book within the arraylist. In this way, we see the details of all books in the booklist
+     *
+     */
     public void displayAllBooks() {
         System.out.println("Details of all books:");
         for(Book currentBook: booklist) {
@@ -91,6 +138,11 @@ public class BookStore
         }
     }
     
+    /**
+     * Method main which contains all statements that should be evaluated when the program is first run
+     *
+     * @param args A parameter containing arguments for the main method code
+     */
     public static void main(String args[]) {
         Book book1 = new Book("The Art of War", "Sun Tzu", 500, 20);
         Book book2 = new Book("The Robots of Dawn", "Isaac Asimov", 1983, 15);
